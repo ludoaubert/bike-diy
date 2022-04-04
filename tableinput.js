65a4d106b18ae0b7c14250a6c72b1020c93ff72b
Ludovic Aubert <ludo.aubert@gmail.com>
2022-04-04 17:52:02 +0200

    Add files via upload

--- tableinput.js
+++ tableinput.js
@@ -26,7 +26,6 @@ input.addEventListener("change", function () {
     
     reader.addEventListener('load', function (e) {
 		mydata = JSON.parse(e.target.result);
-	  //refreshEditDataFromJson(e.target.result);
     });
     
     reader.readAsBinaryString(myFile);
@@ -36,7 +35,6 @@ input.addEventListener("change", function () {
 
 function download(filename) {
   var element = document.createElement('a');
-  //const Json = refreshJsonFromEditData();
   const jsons = prettyData(JSON.stringify(mydata));
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + jsons);
   element.setAttribute('download', filename);
@@ -50,8 +48,6 @@ function download(filename) {
 function download2(filename) {
 	var element = document.createElement('a');
 	const rectangles = compute_box_rectangles(mydata.boxes);
-	//const Json = refreshJsonFromEditData();
-	//const {links, rectangles} = Json;
 	
 	const hex = (i,n) => i.toString(16).padStart(n,'0');
 	
@@ -311,74 +307,6 @@ function updateFieldComment()
 	field2comment[`${box}.${field}`] = fieldCommentTextArea.value;
 }
 
-/*
-function refreshJsonFromEditData()
-{	
-	var boxes = [];
-
-	for (let [id, box] of myBoxes.entries())
-	{
-		boxes.push({"title":box, "id":id, "fields":box2idimage[box]})
-	}
-	
-	let boxComments = [];
-	for (let box in box2comment)
-	{
-		comment = box2comment[box];
-		boxComments.push({box, comment});
-	}
-	
-	let fieldComments = [];
-	for (let boxfield in field2comment)
-	{
-		comment = field2comment[boxfield];
-		[box, field] = boxfield.split(".");
-		fieldComments.push({box, field, comment});
-	}
-
-	let links = [];
-
-	let i=0;
-	for (let option of linkCombo.options)
-	{
-	//Split a string with multiple parameters: Pass in a regexp as the parameter.
-		const [fromBoxTitle, fromFieldName, fromCardinality, toBoxTitle, toFieldName, toCardinality, Category] = option.text.split(/ -> |\./);
-		console.log({fromBoxTitle, fromFieldName, toBoxTitle, toFieldName, Category});
-		const fromBoxIndex = boxes.findIndex( box => box.title == fromBoxTitle );
-		if (fromBoxIndex == -1)
-			alert(`link ${i}: No Box named ${fromBoxTitle}!`);
-		const fromFieldIndex = boxes[fromBoxIndex].fields.findIndex( field => field.name == fromFieldName );
-		if (fromFieldIndex == -1)
-			alert(`link ${i}: Box ${fromBoxTitle} has no field named ${fromFieldName}!`);
-		const toBoxIndex = boxes.findIndex( box => box.title == toBoxTitle );
-		if (toBoxIndex == -1)
-			alert(`link ${i}: No Box named ${toBoxTitle}`);
-		const toFieldIndex = boxes[toBoxIndex].fields.findIndex( field => field.name == toFieldName );
-		if (toFieldIndex == -1)
-			alert(`link ${i}: Box ${toBoxTitle} has no field named ${toFieldName}!`);
-		links.push(
-			{
-			"from":fromBoxIndex, 
-			"fromField":fromFieldIndex,
-			"fromCardinality":fromCardinality,
-			"to":toBoxIndex,
-			"toField":toFieldIndex,
-			"toCardinality":toCardinality,
-			"Category":Category
-			}
-		);
-		i++;
-    }
-	
-	const rectangles = compute_box_rectangles(boxes);
-	const documentTitle = editTitle.value;
-
-	const json = {documentTitle, boxes, boxComments, fieldComments, links, rectangles};
-	return json;
-}
-*/
-
-
 const MONOSPACE_FONT_PIXEL_WIDTH=7;
 const CHAR_RECT_HEIGHT=16;	// in reality 14,8 + 1 + 1 (top and bottom padding) = 16,8
 const RECTANGLE_BOTTOM_CAP=200;
@@ -410,85 +338,6 @@ function compute_box_rectangles(boxes)
 	}
 	return rectangles;
 }
-
-/*
-function refreshEditDataFromJson(Json)
-{
-	const {documentTitle, boxes, values, boxComments, fieldComments, links, fieldColors, rectangles, http_get_param, http_get_request} = JSON.parse(Json);
-	
-	editTitle.value = documentTitle;
-	
-	removeOptions(boxCombo);
-	removeOptions(fieldCombo);
-	removeOptions(fromBoxCombo);
-	removeOptions(toBoxCombo);
-	removeOptions(linkCombo);
-	removeOptions(colorBoxCombo);
-	removeOptions(colorsCombo);
-	
-	myBoxes = [];
-	for (const {title, id, fields} of boxes)
-	{
-		myBoxes.push(title);
-	}
-	
-	console.log(myBoxes);
-	
-	for (const {from,fromField,fromCardinality,to,toField,toCardinality,Category} of links)
-	{
-		console.log({from,fromField,fromCardinality,to,toField,toCardinality,Category});
-		console.assert(from < boxes.length);
-		console.assert(fromField < boxes[from].fields.length);
-		console.assert(to < boxes.length);
-		console.assert(toField < boxes[to].fields.length);
-		
-		let text = boxes[from].title +
-					"." +
-					(fromField != -1 ? boxes[from].fields[fromField].name : '') +
-					"." +
-					fromCardinality +
-					" -> " + 
-					boxes[to].title +
-					"." +
-					(toField != -1 ? boxes[to].fields[toField].name : '') +
-					"." +
-					toCardinality +
-					"." +
-					Category;
-					
-		linkCombo.add(new Option(text, text));
-    }
-	sortSelect(linkCombo);
-
-	for (const box of boxes) 
-	{
-		boxCombo.add(new Option(box.title, box.title));
-	}
-	sortSelect(boxCombo);
-	
-	copyOptions(boxCombo, fromBoxCombo);
-	copyOptions(boxCombo, toBoxCombo);
-	copyOptions(boxCombo, colorBoxCombo);
-	
-	box2comment = {};
-	for (let {box,comment} of boxComments)
-	{
-		box2comment[box] = comment;
-	}
-	
-	field2comment = {};
-	for (let {box,field,comment} of fieldComments)
-	{
-		field2comment[`${box}.${field}`] = comment;
-	}
-	
-	selectBox(boxCombo, fieldCombo);
-	updateFieldAttributes();
-	selectBox(fromBoxCombo, fromFieldCombo);
-	selectBox(toBoxCombo, toFieldCombo);
-	selectField();
-}
-*/
 
 
 function enable_disable()
