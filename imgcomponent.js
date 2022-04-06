7d3182684d93680dedb25f9a21a3b5c16a8add7b
Ludovic Aubert <ludo.aubert@gmail.com>
2022-04-06 17:34:12 +0200

    Add files via upload

--- imgcomponent.js
+++ imgcomponent.js
@@ -7,11 +7,30 @@ function drawComponent(id)
 	if (mypictures==null)
 		mypictures = JSON.parse(pictures);
 	console.log(mypictures);
-	const id_picture = mydata.boxes[id].id_picture;
+	const box = mydata.boxes[id];
+	const id_picture = box.id_picture;
 	console.log(id_picture);
 	const Path = mypictures[id_picture].Path;
 	console.log(Path);
-	return `<img id="${id}" src="${Path}" onmousedown="selectElement(this,'red')" onmouseup="selectElement(this,'green')" onmousemove="moveElement(event)">` ;
+	
+	let innerHTML = `<table id="${id}" onmousedown="selectElement(this,'red')" onmouseup="selectElement(this,'green')" onmousemove="moveElement(event)">`;
+	innerHTML += `<thead>
+					<tr>
+						<th>${box.title}</th>
+					</tr>
+				  </thead>
+				  <tbody>
+				  `;
+
+	innerHTML += `<tr><td>`;
+				  
+	innerHTML += `<img id="${id}" src="${Path}" onmousedown="selectElement(this,'red')" onmouseup="selectElement(this,'green')" onmousemove="moveElement(event)">` ;
+
+	innerHTML += `</td></tr>`;
+
+	innerHTML += `</tbody></table>`;
+	
+	return innerHTML;
 }
 
 
