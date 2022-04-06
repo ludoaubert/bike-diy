4f4741f529fea8e7c292d02d1b33d6f99c987c94
Ludovic Aubert <ludo.aubert@gmail.com>
2022-04-06 12:34:58 +0200

    Add files via upload

--- imgcomponent.js
+++ imgcomponent.js
@@ -6,8 +6,11 @@ function drawComponent(id)
 {
 	if (mypictures==null)
 		mypictures = JSON.parse(pictures);
+	console.log(mypictures);
 	const id_picture = mydata.boxes[id].id_picture;
+	console.log(id_picture);
 	const Path = mypictures[id_picture].Path;
+	console.log(Path);
 	return "<img src=\"" + Path + "\">" ;
 }
 
