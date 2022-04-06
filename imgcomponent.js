b95ec275017ff895d81265383823c7e9f33c47cf
Ludovic Aubert <ludo.aubert@gmail.com>
2022-04-06 18:02:13 +0200

    Add files via upload

--- imgcomponent.js
+++ imgcomponent.js
@@ -1,17 +1,37 @@
 var pictures = null;
 var mypictures = null;
 
+var brands = null;
+var mybrands = null;
+
+var componenttypes = null;
+var mycomponenttypes = null;
+
+var products = null;
+var myproducts = null;
+
 
 function drawComponent(id) 
 {
 	if (mypictures==null)
 		mypictures = JSON.parse(pictures);
+	if (mybrands==null)
+		mybrands = JSON.parse(brands);
+	if (mycomponenttypes==null)
+		mycomponenttypes = JSON.parse(componenttypes);
+	if (myproducts==null)
+		myproducts = JSON.parse(products);
+	
 	console.log(mypictures);
 	const box = mydata.boxes[id];
 	const id_picture = box.id_picture;
 	console.log(id_picture);
-	const Path = mypictures[id_picture].Path;
+	const picture = mypictures[id_picture];
+	const Path = picture.Path;
 	console.log(Path);
+	const product = myproducts[picture.id_product];
+	const brand = mybrands[product.id_brand];
+	const componenttype = mycomponenttypes[product.id_comp];
 	
 	let innerHTML = `<table id="${id}" onmousedown="selectElement(this,'red')" onmouseup="selectElement(this,'green')" onmousemove="moveElement(event)">`;
 	innerHTML += `<thead>
@@ -22,11 +42,21 @@ function drawComponent(id)
 				  <tbody>
 				  `;
 
-	innerHTML += `<tr><td>`;
-				  
-	innerHTML += `<img id="${id}" src="${Path}" onmousedown="selectElement(this,'red')" onmouseup="selectElement(this,'green')" onmousemove="moveElement(event)">` ;
-
+	innerHTML += `<tr><td>`;		  
+		innerHTML += `<img id="${id}" src="${Path}" onmousedown="selectElement(this,'red')" onmouseup="selectElement(this,'green')" onmousemove="moveElement(event)">` ;
 	innerHTML += `</td></tr>`;
+	
+	innerHTML += `<tr><td>`;
+		innerHTML += componenttype.Libelle ;
+	innerHTML += `</td></tr>`;	
+	
+	innerHTML += `<tr><td>`;
+		innerHTML += brand.Libelle ;
+	innerHTML += `</td></tr>`;	
+	
+	innerHTML += `<tr><td>`;
+		innerHTML += product.Libelle ;
+	innerHTML += `</td></tr>`;	
 
 	innerHTML += `</tbody></table>`;
 	
